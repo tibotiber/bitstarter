962278e293d9402da085f1212947871488488989
Thibaut Tiberghien <thibaut@planecq.com>
2014-02-20 10:09:14 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var contents = fs.readFileSync('index.html').toString();
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
